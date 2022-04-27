b6c019c9a41285843c306c354359d660b37f8b81
abhishekchaurasiya <abhichek2580@gmail.com>
2022-04-27 12:21:46 +0530

    get a blog post

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -17,14 +17,40 @@ let createBlog = async (req, res) => {
         if (Object.keys(blogData).length != 0) {
             let saveData = await blogModel.create(blogData);
             res.status(201).send({ message: saveData, status: true });
-        }else{
-            res.status(400).send({ message: "BAD invalid request"});
+        } else {
+            res.status(400).send({ message: "BAD invalid request" });
         }
-        
+
+    } catch (error) {
+        res.status(500).send({ message: "Error", error: error.message });
+    }
+}
+
+let GetBlog = async (req, res) => {
+    try {
+        // let authorId = req.query.authorId;
+        // let category = req.query.category;
+        // let subcategory = req.query.subcategory;
+        // let tags = req.query.tags;
+
+        const { authorId, category, subcategory, tags } = req.query
+
+        let getData = await blogModel.find({ authorId: authorId, category: category, subcategory: subcategory, tags: tags });
+
+        if (!getData) {
+            res.status(400).send({ message: "BAD invalid request" });
+        } else {
+            res.status(200).send({ message: getData, Status: true });
+        }
     } catch (error) {
         res.status(500).send({ message: "Error", error: error.message });
     }
 }
 
 
-module.exports = { createBlog }
+
+
+
+
+
+module.exports = { createBlog, GetBlog }
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 const { createAuthor } = require("../controller/authorController");
-const { createBlog } = require('../controller/blogController');
+const { createBlog, GetBlog} = require('../controller/blogController');
 
 
 // API for createAuthor
 router.post("/authors", createAuthor)
 
 // API for create blogs
-router.post("/blogs", createBlog)
+router.post("/blogs", createBlog);
+router.get("/blogs", GetBlog);
+
+
 
 
 module.exports = router;
